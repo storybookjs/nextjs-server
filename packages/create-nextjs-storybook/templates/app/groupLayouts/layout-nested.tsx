a6d3d6e0e1f96f48e6eb55ce4eead063e29c4330
Michael Shilman <shilman@users.noreply.github.com>
2023-12-08 12:27:15 +0800

    Duplicate app layouts in CLI rather than link

---
+++ packages/create-nextjs-storybook/templates/app/groupLayouts/layout-nested.tsx
@@ -0,0 +1,6 @@
+import type { PropsWithChildren } from 'react';
+import React from 'react';
+
+export default function NestedLayout({ children }: PropsWithChildren<{}>) {
+  return <>{children}</>;
+}
